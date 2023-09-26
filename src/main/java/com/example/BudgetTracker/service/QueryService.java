8e7cb8fa863b27e9c7d59eed9f9768ccc4cd04b0
Patrick <patrickslaine@yahoo.co.uk>
2023-09-26 15:00:31 +0000

    resolve merge conflict

--- src/main/java/com/example/BudgetTracker/service/QueryService.java
+++ src/main/java/com/example/BudgetTracker/service/QueryService.java
@@ -1,40 +1,14 @@
-<<<<<<< HEAD
-//package com.example.BudgetTracker.service;
-//
-//import com.example.BudgetTracker.repository.BudgetRepository;
-//import com.example.BudgetTracker.repository.ExpenseRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Dictionary;
-//
-//@Service
-//public class QueryService {
-//
-//    @Autowired
-//    private BudgetRepository budgetRepository;
-//
-//    @Autowired
-//    private ExpenseRepository expenseRepository;
-//
-//    public QueryService(BudgetRepository budgetRepository, ExpenseRepository expenseRepository){
-//        this.budgetRepository = budgetRepository;
-//        this.expenseRepository= expenseRepository;
-//    }
-//
-//    public Dictionary getExpenseInfo(String category){
-//
-//    }
-//}
-=======
+
 package com.example.BudgetTracker.service;
 
 import com.example.BudgetTracker.model.entities.Budget;
 import com.example.BudgetTracker.model.entities.Expense;
+import com.example.BudgetTracker.repository.BudgetRepository;
+import com.example.BudgetTracker.repository.ExpenseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import java.util.Dictionary;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -80,8 +54,3 @@ public class QueryService {
     }
 }
 
-
-
-
-
->>>>>>> bec1781 (Query)
