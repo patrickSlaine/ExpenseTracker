ec4f48fd6ad8ac11ae4b7e571d5fa6faa22418d7
Nayima <nayimaurooj@gmail.com>
2023-09-26 13:14:14 +0000

    Testing

--- src/test/java/com/example/BudgetTracker/controller/ExpenseControllerTest.java
+++ src/test/java/com/example/BudgetTracker/controller/ExpenseControllerTest.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@ActiveProfiles({})
+@ActiveProfiles({"dbseeder"})
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles({"h2","dbseeder"})
